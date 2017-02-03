abdb202492c5cf4dea7713d89ee643eed2dd6b13
Devon Kinghorn <devonkinghorn@byu716013wks.app.byu.edu>
2017-02-03 12:50:13 -0700

    connects to database

--- src/main/java/edu/goldenhammer/model/DatabaseConnnectionFactory.java
+++ src/main/java/edu/goldenhammer/model/DatabaseConnnectionFactory.java
@@ -3,10 +3,96 @@ package edu.goldenhammer.model;
 import org.apache.commons.dbcp2.BasicDataSource;
 import javax.sql.DataSource;
 import java.sql.*;
+
 /**
  * Created by devonkinghorn on 2/1/17.
  */
 public class DatabaseConnnectionFactory {
+//    private static final String POSTGRESQL_DRIVER_NAME = "org.postgresql.Driver";
+//
+//    private DataSource dataSource;
+//    private String schema;
+//    private String databaseName;
+
+    private static DatabaseConnnectionFactory singleton;
+    public static DatabaseConnnectionFactory getInstance() {
+        if (singleton == null) {
+            String host = "ec2-35-167-43-52.us-west-2.compute.amazonaws.com";
+            String database = "postgres";
+            String schema = null;
+            int maxConnections = 10;
+            String userName = "postgres";
+            String password = "goldenhammerteam";
+            singleton = new DatabaseConnnectionFactory(host, database, schema, userName, password);
+        }
+        return singleton;
+    }
+
+//    DatabaseConnnectionFactory(String host, String database, String schema, int maxConnections, String userName, String password) {
+//        this.databaseName = database;
+//        this.dataSource = configureDataSource(host, database, maxConnections, userName, password);
+//        if (schema != null) {
+//            this.schema = schema;
+//        }
+//        else {
+//            this.schema = "public";
+//        }
+//    }
+////private static final String POSTGRESQL_DRIVER_NAME = "org.postgresql.Driver";
+////    private String schema;
+////    private String url;
+////    private String userName;
+////    private String password;
+////public DatabaseConnnectionFactory(String host, String database, String schema, String userName, String password) {
+////    this.url = String.format("jdbc:postgresql://%s:5432/%s", host, database);
+////    this.schema = schema;
+////    this.userName = userName;
+////    this.password = password;
+////    try {
+////        Class.forName(POSTGRESQL_DRIVER_NAME);
+////    }
+////    catch (ClassNotFoundException e) {
+////        e.printStackTrace();
+////    }
+////}
+////
+//////    @Override
+////    public Connection getConnection() {
+////        try {
+////            Connection conn = DriverManager.getConnection(url, userName, password);
+////            if (schema != null) {
+////                conn.setSchema(schema);
+////            }
+////            return conn;
+////        }
+////        catch (SQLException e) {
+////            throw new RuntimeException("Failed to get connection");
+////        }
+////    }
+//
+//    public Connection getConnection() {
+//        try {
+//            Connection conn = dataSource.getConnection();
+//            conn.setSchema(schema);
+//            return conn;
+//        }
+//        catch (SQLException e) {
+//            throw new RuntimeException(String.format("Failed to get connection to database: %s schema: %s", databaseName, schema), e);
+//        }
+//    }
+//
+//    private DataSource configureDataSource(String host, String database, int maxConnections, String userName, String password) {
+//
+//        String url = String.format("jdbc:postgresql://%s:5432/%s", host, database);
+//        BasicDataSource dataSource = new BasicDataSource();
+//        dataSource.setDriverClassName(POSTGRESQL_DRIVER_NAME);
+//        dataSource.setUrl(url);
+//        dataSource.setUsername(userName);
+//        dataSource.setPassword(password);
+//        dataSource.setMaxTotal(maxConnections);
+//        return dataSource;
+//    }
+
     private static final String POSTGRESQL_DRIVER_NAME = "org.postgresql.Driver";
     private String schema;
     private String url;
@@ -25,7 +111,6 @@ public class DatabaseConnnectionFactory {
             e.printStackTrace();
         }
     }
-
 
     public Connection getConnection() {
         try {
