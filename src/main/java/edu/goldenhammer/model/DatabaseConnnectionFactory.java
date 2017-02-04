42d197a718bdfabd7014d4053099f03377b9838b
Devon Kinghorn <devonkinghorn@null-76dfb1942e85.app.byu.edu>
2017-02-04 11:00:11 -0700

    pooledConnectionFactory

--- src/main/java/edu/goldenhammer/model/DatabaseConnnectionFactory.java
+++ src/main/java/edu/goldenhammer/model/DatabaseConnnectionFactory.java
@@ -23,105 +23,47 @@ public class DatabaseConnnectionFactory {
             int maxConnections = 10;
             String userName = "postgres";
             String password = "goldenhammerteam";
-            singleton = new DatabaseConnnectionFactory(host, database, schema, userName, password);
+            singleton = new DatabaseConnnectionFactory(host, database, schema, maxConnections, userName, password);
         }
         return singleton;
     }
 
-//    DatabaseConnnectionFactory(String host, String database, String schema, int maxConnections, String userName, String password) {
-//        this.databaseName = database;
-//        this.dataSource = configureDataSource(host, database, maxConnections, userName, password);
-//        if (schema != null) {
-//            this.schema = schema;
-//        }
-//        else {
-//            this.schema = "public";
-//        }
-//    }
-////private static final String POSTGRESQL_DRIVER_NAME = "org.postgresql.Driver";
-////    private String schema;
-////    private String url;
-////    private String userName;
-////    private String password;
-////public DatabaseConnnectionFactory(String host, String database, String schema, String userName, String password) {
-////    this.url = String.format("jdbc:postgresql://%s:5432/%s", host, database);
-////    this.schema = schema;
-////    this.userName = userName;
-////    this.password = password;
-////    try {
-////        Class.forName(POSTGRESQL_DRIVER_NAME);
-////    }
-////    catch (ClassNotFoundException e) {
-////        e.printStackTrace();
-////    }
-////}
-////
-//////    @Override
-////    public Connection getConnection() {
-////        try {
-////            Connection conn = DriverManager.getConnection(url, userName, password);
-////            if (schema != null) {
-////                conn.setSchema(schema);
-////            }
-////            return conn;
-////        }
-////        catch (SQLException e) {
-////            throw new RuntimeException("Failed to get connection");
-////        }
-////    }
-//
-//    public Connection getConnection() {
-//        try {
-//            Connection conn = dataSource.getConnection();
-//            conn.setSchema(schema);
-//            return conn;
-//        }
-//        catch (SQLException e) {
-//            throw new RuntimeException(String.format("Failed to get connection to database: %s schema: %s", databaseName, schema), e);
-//        }
-//    }
-//
-//    private DataSource configureDataSource(String host, String database, int maxConnections, String userName, String password) {
-//
-//        String url = String.format("jdbc:postgresql://%s:5432/%s", host, database);
-//        BasicDataSource dataSource = new BasicDataSource();
-//        dataSource.setDriverClassName(POSTGRESQL_DRIVER_NAME);
-//        dataSource.setUrl(url);
-//        dataSource.setUsername(userName);
-//        dataSource.setPassword(password);
-//        dataSource.setMaxTotal(maxConnections);
-//        return dataSource;
-//    }
-
-    private static final String POSTGRESQL_DRIVER_NAME = "org.postgresql.Driver";
-    private String schema;
-    private String url;
-    private String userName;
-    private String password;
-
-    public DatabaseConnnectionFactory(String host, String database, String schema, String userName, String password) {
-        this.url = String.format("jdbc:postgresql://%s:5432/%s", host, database);
-        this.schema = schema;
-        this.userName = userName;
-        this.password = password;
-        try {
-            Class.forName(POSTGRESQL_DRIVER_NAME);
+    DatabaseConnnectionFactory(String host, String database, String schema, int maxConnections, String userName, String password) {
+        this.databaseName = database;
+        this.dataSource = configureDataSource(host, database, maxConnections, userName, password);
+        if (schema != null) {
+            this.schema = schema;
         }
-        catch (ClassNotFoundException e) {
-            e.printStackTrace();
+        else {
+            this.schema = "public";
         }
     }
 
+    private static final String POSTGRESQL_DRIVER_NAME = "org.postgresql.Driver";
+    private DataSource dataSource;
+    private String schema;
+    private String databaseName;
+
     public Connection getConnection() {
         try {
-            Connection conn = DriverManager.getConnection(url, userName, password);
-            if (schema != null) {
-                conn.setSchema(schema);
-            }
+            Connection conn = dataSource.getConnection();
+            conn.setSchema(schema);
             return conn;
         }
         catch (SQLException e) {
-            throw new RuntimeException("Failed to get connection");
+            throw new RuntimeException(String.format("Failed to get connection to database: %s schema: %s", databaseName, schema), e);
         }
+    }
+
+    private DataSource configureDataSource(String host, String database, int maxConnections, String userName, String password) {
+
+        String url = String.format("jdbc:postgresql://%s:5432/%s", host, database);
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName(POSTGRESQL_DRIVER_NAME);
+        dataSource.setUrl(url);
+        dataSource.setUsername(userName);
+        dataSource.setPassword(password);
+        dataSource.setMaxTotal(maxConnections);
+        return dataSource;
     }
 }
