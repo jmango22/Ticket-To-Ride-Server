3c78b28d12322e76cf6786b312666681daabf0df
Sean Beecroft <seanjib@gmail.com>
2017-02-11 10:57:53 -0700

    added login functionality to DatabaseController

--- src/main/java/edu/goldenhammer/database/DatabaseController.java
+++ src/main/java/edu/goldenhammer/database/DatabaseController.java
@@ -152,6 +152,21 @@ public class DatabaseController implements IDatabaseController {
      */
     @Override
     public Boolean login(String username, String password) {
+        try (Connection connection = session.getConnection()) {
+            String sqlString = String.format("SELECT %1$s FROM %2$s where %3$s=? and %4$s=?",
+                    ServerPlayer.columnNames(),
+                    ServerPlayer.TABLE_NAME,
+                    ServerPlayer.USERNAME,
+                    ServerPlayer.PASSWORD);
+            PreparedStatement statement = connection.prepareStatement(sqlString);
+            statement.setString(1,username);
+            statement.setString(2,password);
+            ResultSet resultSet = statement.executeQuery();
+
+            return !resultSet.next(); //if there is one result for the username and password input, it is a valid login
+        } catch(SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
