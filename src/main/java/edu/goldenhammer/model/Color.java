ec39d016649c67722c5a2825db70cfd34c810bcd
Sean Beecroft <seanjib@gmail.com>
2017-03-05 15:42:02 -0700

    Calling playGame now populates the database if the game has not been started yet, and then returns the proper game model (whether or not the game has just started)

--- src/main/java/edu/goldenhammer/model/Color.java
+++ src/main/java/edu/goldenhammer/model/Color.java
@@ -4,5 +4,72 @@ package edu.goldenhammer.model;
  * Created by seanjib on 3/2/2017.
  */
 public enum Color {
-    WILD, RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, BLACK, WHITE
+    WILD, RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE, BLACK, WHITE, GRAY;
+
+    public static Color getPlayerColorFromNumber(int colorNumber) {
+        switch(colorNumber) {
+            case 1:
+                return BLUE;
+            case 2:
+                return RED;
+            case 3:
+                return GREEN;
+            case 4:
+                return YELLOW;
+            case 5:
+                return BLACK;
+            default:
+                return null;
+        }
+    }
+
+    public static Color getTrainCardColorFromString(String color) {
+        switch (color) {
+            case "wild":
+                return WILD;
+            case "red":
+                return RED;
+            case "orange":
+                return ORANGE;
+            case "yellow":
+                return YELLOW;
+            case"green":
+                return GREEN;
+            case "blue":
+                return BLUE;
+            case "violet":
+                return PURPLE;
+            case "black":
+                return BLACK;
+            case "white":
+                return WHITE;
+            default:
+                return null;
+        }
+    }
+
+    public static Color getTrackColorFromString(String color) {
+        switch (color) {
+            case "gray":
+                return GRAY;
+            case "red":
+                return RED;
+            case "orange":
+                return ORANGE;
+            case "yellow":
+                return YELLOW;
+            case"green":
+                return GREEN;
+            case "blue":
+                return BLUE;
+            case "violet":
+                return PURPLE;
+            case "black":
+                return BLACK;
+            case "white":
+                return WHITE;
+            default:
+                return null;
+        }
+    }
 }
--- src/main/java/edu/goldenhammer/model/GameModel.java
+++ src/main/java/edu/goldenhammer/model/GameModel.java
@@ -6,40 +6,57 @@ import java.util.List;
  * Created by seanjib on 2/11/2017.
  */
 public class GameModel implements IGameModel {
-    public GameModel(String id, String name, boolean started, List<String> players){
-        this.id = id;
-        this.name = name;
-        this.started = started;
+    private List<PlayerOverview> players;
+    private List<DestinationCard> destinationDeck;
+    private List<TrainCard> trainCardDeck;
+    private Map map;
+    private GameName name;
+
+    public GameModel(List<PlayerOverview> players, List<DestinationCard> destinationDeck, List<TrainCard> trainCardDeck, Map map, GameName name) {
         this.players = players;
+        this.destinationDeck = destinationDeck;
+        this.trainCardDeck = trainCardDeck;
+        this.map = map;
+        this.name = name;
     }
 
-
-    public String getID() {
-        return id;
-    }
-
-
-    public String getName() {
-        return name;
-    }
-
-    public boolean isStarted() {
-        return started;
-    }
-
-    public List<String> getPlayers() {
+    public List<PlayerOverview> getPlayers() {
         return players;
     }
 
-    @Override
-    public void setPlayers(List<String> players) { this.players = players; }
+    public void setPlayers(List<PlayerOverview> players) {
+        this.players = players;
+    }
 
-    private String id;
-    private String name;
-    private boolean started;
-    private List<String> players;
-    private Map map;
+    public List<DestinationCard> getDestinationDeck() {
+        return destinationDeck;
+    }
 
+    public void setDestinationDeck(List<DestinationCard> destinationDeck) {
+        this.destinationDeck = destinationDeck;
+    }
 
+    public List<TrainCard> getTrainCardDeck() {
+        return trainCardDeck;
+    }
 
+    public void setTrainCardDeck(List<TrainCard> trainCardDeck) {
+        this.trainCardDeck = trainCardDeck;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public void setMap(Map map) {
+        this.map = map;
+    }
+
+    public GameName getName() {
+        return name;
+    }
+
+    public void setName(GameName name) {
+        this.name = name;
+    }
 }
