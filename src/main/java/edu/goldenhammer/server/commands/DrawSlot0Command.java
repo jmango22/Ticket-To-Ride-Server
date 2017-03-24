9bbb861d4d19985bda90bae8d5fc47b5b6b9d621
seanjib <seanjib@gmail.com>
2017-03-24 12:48:35 -0600

    Revert "Drawtraincards"

---
+++ src/main/java/edu/goldenhammer/server/commands/DrawSlot0Command.java
@@ -0,0 +1,13 @@
+package edu.goldenhammer.server.commands;
+
+import edu.goldenhammer.server.Results;
+
+/**
+ * Created by seanjib on 3/4/2017.
+ */
+public class DrawSlot0Command extends DrawSlotCommand {
+    @Override
+    public Results execute() {
+        return null;
+    }
+}
---
+++ src/main/java/edu/goldenhammer/server/commands/DrawTopCommand.java
@@ -0,0 +1,13 @@
+package edu.goldenhammer.server.commands;
+
+import edu.goldenhammer.server.Results;
+
+/**
+ * Created by seanjib on 3/4/2017.
+ */
+public class DrawTopCommand extends DrawDestCardsCommand {
+    @Override
+    public Results execute() {
+        return new Results();
+    }
+}
