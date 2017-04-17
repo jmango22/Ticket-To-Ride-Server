178dcdf927ca7baacc718013d746c07efef9d16a
Sean Beecroft <seanjib@gmail.com>
2017-04-17 11:58:15 -0600

    added slot-drawing functionality. Added shuffling train cards function.

--- src/main/java/edu/goldenhammer/model/GameModel.java
+++ src/main/java/edu/goldenhammer/model/GameModel.java
@@ -80,4 +80,12 @@ public class GameModel implements IGameModel {
     public void setName(GameName name) {
         this.name = name;
     }
+
+    public List<Color> getBank() {
+        return mBank;
+    }
+
+    public void setBank(List<Color> mBank) {
+        this.mBank = mBank;
+    }
 }
