9f0fe22b5db0d5431c0d009f493fbe23fce72a81
Sean Beecroft <seanjib@gmail.com>
2017-04-17 17:41:43 -0600

    successfully broken into IGameDAO and IUserDAO

--- src/main/java/edu/goldenhammer/database/SQLGameDAO.java
+++ src/main/java/edu/goldenhammer/database/SQLGameDAO.java
@@ -1,8 +1,178 @@
 package edu.goldenhammer.database;
 
+import edu.goldenhammer.model.*;
+import edu.goldenhammer.server.commands.BaseCommand;
+import edu.goldenhammer.server.commands.EndTurnCommand;
+
+import java.util.List;
+
 /**
  * Created by McKean on 4/17/2017.
  */
 
-public class SQLGameDAO {
+public class SQLGameDAO implements IGameDAO{
+    @Override
+    public Boolean createGame(String name) {
+        return null;
+    }
+
+    @Override
+    public GameList getGames() {
+        return null;
+    }
+
+    @Override
+    public GameList getGames(String player) {
+        return null;
+    }
+
+    @Override
+    public Boolean joinGame(String username, String game_name) {
+        return null;
+    }
+
+    @Override
+    public Boolean leaveGame(String username, String game_name) {
+        return null;
+    }
+
+    @Override
+    public void maybeDropGame(String gameName) {
+
+    }
+
+    @Override
+    public IGameModel playGame(String game_name) {
+        return null;
+    }
+
+    @Override
+    public boolean allHandsInitialized(String gameName) {
+        return false;
+    }
+
+    @Override
+    public TrainCard drawRandomTrainCard(String gameName, String playerName) {
+        return null;
+    }
+
+    @Override
+    public TrainCard drawTrainCardFromSlot(String game_name, String player_name, int slot) {
+        return null;
+    }
+
+    @Override
+    public boolean hasDrawnTwoTrainCards(String game_name, String player_name) {
+        return false;
+    }
+
+    @Override
+    public void redealSlotCards(String game_name) {
+
+    }
+
+    @Override
+    public boolean discardCard(String gameName, String playerName, Color color) {
+        return false;
+    }
+
+    @Override
+    public List<Color> getSlotCardColors(String game_name) {
+        return null;
+    }
+
+    @Override
+    public DestinationCard drawRandomDestinationCard(String gameName, String playerName) {
+        return null;
+    }
+
+    @Override
+    public List<DestinationCard> getPlayerDestinationCards(String game_name, String player_name) {
+        return null;
+    }
+
+    @Override
+    public boolean returnDestCards(String gameName, String playerName, List<DestinationCard> destinationCards) {
+        return false;
+    }
+
+    @Override
+    public boolean postMessage(String game_name, String player_name, String message) {
+        return false;
+    }
+
+    @Override
+    public List<Message> getMessages(String game_name) {
+        return null;
+    }
+
+    @Override
+    public boolean canClaimRoute(String game_name, String username, int route_number) {
+        return false;
+    }
+
+    @Override
+    public boolean claimRoute(String game_name, String username, int route_number) {
+        return false;
+    }
+
+    @Override
+    public void removeTrainsFromPlayer(String game_name, String username, int trainsToRemove) {
+
+    }
+
+    @Override
+    public List<Track> getTracks(String game_name) {
+        return null;
+    }
+
+    @Override
+    public int numTrainsLeft(String game_name, String player_name) {
+        return 0;
+    }
+
+    @Override
+    public boolean addCommand(BaseCommand cmd, boolean visibleToSelf, boolean visibleToAll) {
+        return false;
+    }
+
+    @Override
+    public EndTurnCommand getEndTurnCommand(String gameName, int commandNumber, String playerName) {
+        return null;
+    }
+
+    @Override
+    public List<BaseCommand> getCommandsSinceLastCommand(String game_name, String player_name, int lastCommandID) {
+        return null;
+    }
+
+    @Override
+    public boolean validateCommand(BaseCommand command) {
+        return false;
+    }
+
+    @Override
+    public int getNumberOfDrawTrainCommands(String game_name) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEndOfGame(String game_name) {
+        return false;
+    }
+
+    @Override
+    public boolean alreadyLastRound(String game_name) {
+        return false;
+    }
+
+    @Override
+    public GameModel getGameModel(String game_name) {
+        return null;
+    }
+
+    @Override
+    public void updateCurrentPlayer(String game_name, int nextPlayer) {
+
+    }
 }
